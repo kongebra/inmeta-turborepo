139407dd3843661de3a6b4172f8a45b03d349be0
Svein Are Danielsen <svein.danielsen@inmeta.no>
2024-01-24 14:05:14 +0100

    feat: better headers

--- apps/inmeta-games-web/next.config.mjs
+++ apps/inmeta-games-web/next.config.mjs
@@ -2,42 +2,64 @@ const cspHeader = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
+    img-src 'self' cdn.sanity.io blob: data:;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'none';
+    media-src 'self' cdn.sanity.io;
+    connect-src 'self' https://jhhsx3kh.apicdn.sanity.io wss://jhhsx3kh.api.sanity.io cdn.sanity.io;
     block-all-mixed-content;
     upgrade-insecure-requests;
 `;
+
+const securityHeaders = [
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "no-referrer-when-downgrade",
+  },
+  {
+    key: "Access-Control-Allow-Origin",
+    value: "*",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: cspHeader.replace(/\s{2,}/g, " ").trim(),
+  },
+];
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
 
+  images: {
+    remotePatterns: [
+      "https://cdn.sanity.io/images/**", // Sanity.io images
+    ],
+  },
+
   async headers() {
     return [
       {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ""),
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-        ],
+        source: "/:path*",
+        headers: securityHeaders,
       },
     ];
   },
