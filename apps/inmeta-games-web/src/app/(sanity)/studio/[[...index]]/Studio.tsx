55b11bb501eb7c04fd7f311e8d65627fcffec8c4
Svein Are Danielsen <svein.danielsen@inmeta.no>
2024-06-06 15:50:32 +0200

    refactor: upgrade and fix breakinc change

--- apps/inmeta-games-web/src/app/(sanity)/studio/[[...index]]/Studio.tsx
+++ apps/inmeta-games-web/src/app/(sanity)/studio/[[...index]]/Studio.tsx
@@ -1,17 +1,11 @@
+// ./src/app/studio/[[...index]]/Studio.tsx
 "use client";
 
-/**
- * This route is responsible for the built-in authoring environment using Sanity Studio.
- * All routes under your studio path is handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
- *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
- */
-
 import { NextStudio } from "next-sanity/studio";
+
 import config from "../../../../../sanity.config";
 
-export default function Studio() {
+export function Studio() {
+  //  Supports the same props as `import {Studio} from 'sanity'`, `config` is required
   return <NextStudio config={config} />;
 }
